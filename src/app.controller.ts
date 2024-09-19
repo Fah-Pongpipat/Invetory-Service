512c851a2247020aba0d1f4c516d72a30f2751c3
Fah-Pongpipat <163412313+Fah-Pongpipat@users.noreply.github.com>
2024-09-19 11:49:14 +0700

    Set up Message Pattern

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,5 +1,11 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import {
+  Ctx,
+  MessagePattern,
+  Payload,
+  RmqContext,
+} from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
@@ -8,5 +14,23 @@ export class AppController {
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @MessagePattern('Order_created')
+  handleOrderCreated(@Payload() data: any, @Ctx() context: RmqContext) {
+    // console.log(`Pattern: ${context.getPattern}`);
+    // console.log(context.getMessage());
+    // console.log(context.getChannelRef());\
+    console.log('Order recieved for processing', data);
+    const channel = context.getChannelRef();
+    const originalMsg = context.getMessage();
+    const isInStock = true;
+    if (isInStock) {
+      console.log('Inventory available, Processing Order');
+      channel.ack(originalMsg);
+    } else {
+      console.log('Inventory not available');
+      channel.ack(originalMsg);
+    }
   }
 }
